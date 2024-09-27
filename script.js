591bb4fadf15b2b4455422143fffc5612611bd28
Kalyan3081 <162336115+Kalyan3081@users.noreply.github.com>
2024-09-27 22:27:06 +0530

    created a submission

--- script.js
+++
@@ -1,9 +0,0 @@
-//your JS code here. If required.
-$(document).ready(function() {
-    $('.lightbox').magnificPopup({
-        type: 'image',
-        gallery: {
-            enabled: true // Enables gallery mode
-        }
-    });
-});
